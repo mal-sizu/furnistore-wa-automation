67cc693a34c13d45acf5785df162c20793b3536b
Yunal Dewnith <93931829+YaraAstro@users.noreply.github.com>
2025-07-23 22:17:20 +0530

    Add verify certificates

--- app/(main)/layout.tsx
+++
@@ -1,8 +0,0 @@
-"use client"
-
-import ClientLayout from "@/layouts/main-layout"
-import type { ReactNode } from "react"
-
-export default function MainLayout({ children }: { children: ReactNode }) {
-  return <ClientLayout>{children}</ClientLayout>
-}
